b2b29546ed6d05e86c182fb0d643a2b21d731f2e
Chris Friemann <github@chris.friemann.nl>
2023-10-02 11:13:47 +0200

    :tada: added checkRound function for starting over

--- src/main/java/nl/chris/GameRound.java
+++ src/main/java/nl/chris/GameRound.java
@@ -31,5 +31,28 @@ public class GameRound extends JPanel {
     public void addRound() {
         round++;
         roundLabel.setText("Round: " + round);
+        checkRound();
+    }
+
+    /**
+     * Check if round is 6, if so, show dialog with score and ask if player wants to play again
+     */
+    public void checkRound() {
+        if (round == 6) {
+            int dialogButton = JOptionPane.YES_NO_OPTION;
+            int dialogResult = JOptionPane.showConfirmDialog(null, "You have finished the game!\nYour score is: " + wordList.gameScore.score + "\nDo you want to play again?", "Game finished", dialogButton);
+
+            // if yes, reset score and round
+            if (dialogResult == JOptionPane.YES_OPTION) {
+                wordList.gameScore.resetScore();
+                round = 1;
+                roundLabel.setText("Round: " + round);
+            }
+
+            // if no, exit game
+            else {
+                System.exit(0);
+            }
+        }
     }
 }
